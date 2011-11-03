f804ab41c9d2d9a04448ff77d7df020257708a87
dtao <daniel.tao@gmail.com>
2011-11-03 10:18:15 -0700

    forgot to include node file in previous commit

--- lib/node.rb
+++ lib/node.rb
@@ -1,9 +1,31 @@
+require File.dirname(__FILE__) + '/indentation'
+require File.dirname(__FILE__) + '/xml_node_formatter'
+
 class Node
+  include Indentation
+
+  @@xml_formatter = XmlNodeFormatter.new
+
   attr_reader :label
   attr_reader :children
 
   def initialize(label)
     @label = label
     @children = []
+  end
+
+  def to_s(indent_level = 0)
+    s = indent(@label, indent_level)
+
+    @children.each do |child|
+      s << "\n"
+      s << indent(child.to_s, indent_level + 1)
+    end
+
+    s
+  end
+
+  def to_xml
+    @@xml_formatter.format self
   end
 end
